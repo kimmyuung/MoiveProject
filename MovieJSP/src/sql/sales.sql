ff1da83d04ff59a2fcaf67474f445684d364db92
김명호 <rlaaudgh1003@daum.net>
2022-05-09 21:06:10 +0900

    Delete MovieJSP directory

--- MovieJSP/src/sql/sales.sql
+++
@@ -1,8 +0,0 @@
-create database movieproject; 
-
-
-/* 테이블 생성 */
-create table movieproject.sales(
-	sales int,
-	sdate datetime
-);
--- MovieJSP/src/sql/theater.sql
+++
@@ -1,9 +0,0 @@
-create database movieproject; 
-
-
-/* 테이블 생성 */
-create table movieproject.theater(
-	tnum int primary key,
-	tname varchar(30),
-	tseat varchar(200)
-);
